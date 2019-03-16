7d4b68722049feced96af56a2fb283e39841bab5
‘RSpidla’ <rspidla@gmail.com>
2019-03-16 18:35:50 +0000

    Exercise 2: Comments and Pound Characters

---
+++ ex02.rb
@@ -0,0 +1,18 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
+
+# Test code for ruby strings
+
+#!/usr/bin/ruby
+
+testString = String.new("Or I could have code like this.")
+foo = testString.downcase
+
+puts "#{foo}"
