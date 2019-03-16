d0feae2a499ba08caf67c1942c0137bd479b36d3
‘RSpidla’ <rspidla@gmail.com>
2019-03-16 18:37:40 +0000

    Exercise 6: Strings and Text

---
+++ ex06.rb
@@ -0,0 +1,45 @@
+
+# Set variable
+types_of_people = 10
+
+# Set variable with string
+x = "There are #{types_of_people} types of people."
+
+# Set variable
+binary = "binary"
+
+# Set variable
+do_not = "don't"
+
+# Set variable with 2 strings
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Display variable
+puts x
+
+# Display variable
+puts y
+
+# Display variable in string
+puts "I said: #{x}."
+
+# Display variable in string
+puts "I also said: #{y}."
+
+# Set boolean variable
+hilarious = false
+
+# Set variable with string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Display variable
+puts joke_evaluation
+
+# Set variable
+w = "This is the left side of..."
+
+# Set variable
+e = "a string with a right side."
+
+# Display 2 variables using concatenation
+puts w + e
