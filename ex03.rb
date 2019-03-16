213c924b693093d4ce4ca796414a2781f679961e
‘RSpidla’ <rspidla@gmail.com>
2019-03-16 18:36:20 +0000

    Exercise 3: Numbers and Math

---
+++ ex03.rb
@@ -0,0 +1,41 @@
+# Prints "I will now count my chickens:"
+puts "I will now count my chickens:"
+
+# Does computation
+puts "Hens #{25 + 30 / 6}"
+
+# Does computation
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Prints "Now I will count the eggs:"
+puts "Now I will count the eggs:"
+
+# Does computation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Does computation
+puts 3 + 2 < 5 - 7
+
+# Prints "What is 3 + 2?" and does computation
+puts "What is 3 + 2? #{3 + 2}"
+
+# Prints "What is 5 - 7?" and does computation
+puts "What is 5 - 7? #{5 - 7}"
+
+# Prints "Oh, that's why it's false."
+puts "Oh, that's why it's false."
+
+# Prints "How about some more."
+puts "How about some more."
+
+# Prints "Is it greater?" and does computation
+puts "Is it greater? #{5 > -2}"
+
+# Prints "Is it greater or equal?" and does computation
+puts "Is it greater or equal? #{5 >= -2}"
+
+# Prints "Is it less or equal?" and does computation
+puts "Is it less or equal? #{5 <= -2}"
